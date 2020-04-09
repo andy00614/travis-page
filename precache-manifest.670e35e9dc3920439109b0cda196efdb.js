366d5bada695dc3ccc27b711fe4a0bec9e238c4b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-09 04:47:57 +0000

    Deploy andy00614/travis-page to github.com/andy00614/travis-page.git:gh-pages

---
+++ precache-manifest.670e35e9dc3920439109b0cda196efdb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c01c5b40868425604f696e83ad7022d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1374540e25e1308b5fa6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3dc1a43fb33ed1f524c6",
+    "url": "/static/js/2.c4dafd94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c4dafd94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1374540e25e1308b5fa6",
+    "url": "/static/js/main.e9faa7cc.chunk.js"
+  },
+  {
+    "revision": "ec66328604093ad86f8a",
+    "url": "/static/js/runtime-main.9817a57a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
